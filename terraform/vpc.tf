af1d94fdc94996c360e0817cb01b850e6a6ca9af
Shaardeep <95283216+Shaardeep@users.noreply.github.com>
2026-02-03 17:26:18 +0000

    add new ec2

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,14 +9,26 @@ resource "aws_vpc" "tVPC"{
 #step 2 : Create a public subnet
 resource "aws_subnet" "PublicSubnet"{
 	vpc_id = aws_vpc.tVPC.id
-	cidr_block = "170.0.0.1/24"
+	cidr_block = "170.0.1.0/24"
 }
 
 #step 3 : Create a private subnet
 resource "aws_subnet" "PrivateSubnet"{
         vpc_id = aws_vpc.tVPC.id
-        cidr_block = "170.0.3.0/24"
+        cidr_block = "170.0.2.0/24"
 }
+
+#step4 : create 1 new public subnet
+resource "aws_subnet" "PublicSubnet_1b"{
+	vpc_id = aws_vpc.tVPC.id
+	cidr_block = "170.0.3.0/24"
+	availability_zone = "ap-south-1b"
+
+	tags = {
+		Name = "public-subnet-1b"
+	}
+}
+
 #step 4 : Create IGW
 resource "aws_internet_gateway" "igw"{
 	vpc_id = aws_vpc.tVPC.id
@@ -26,13 +38,13 @@ resource "aws_route_table" "PublicRT"{
 	vpc_id = aws_vpc.tVPC.id
 	route {
 		cidr_block = "0.0.0.0/0"
-		gateway_id = "aws_internet_gateway.igw.id"
+		gateway_id = aws_internet_gateway.igw.id
 	}
 }
  
 #step 6 : route table associate with public subnet
 resource "aws_route_table_association" "PublicRTA"{
-	subnet_id = aws_subnet.PublicSubnet.id
+	subnet_id = aws_subnet.PublicSubnet_1b.id
 	route_table_id =  aws_route_table.PublicRT.id
 
 }
